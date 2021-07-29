c81385af53daab9040c2a448b853d4ee7a9afabc
”Aleksandr <Kunsanba@gmail.com>
2021-07-29 10:48:01 +0300

    Error correction

--- src/by/tms/Task1.java
+++ src/by/tms/Task1.java
@@ -14,13 +14,13 @@ public class Task1 {
     public static void main(String[] args) {
 
         int number = 2140000000;
-        double result1 = (double)number*number*number;
+        double result1 = (double) number * number * number;
         System.out.println("Double Res = " + result1);
 
         var result2 = Math.pow(number, 3);
         System.out.println("Math Res = " + result2);
 
-        var result4 = (double)number*number*number;
+        var result4 = (double) number * number * number;
         System.out.println("Var Res = " + result4);
     }
 }
