ffe1e70d77feae7803830ca5833a043f9d276472
Aleksandr Kunevich <87695606+AleksandrKunevich@users.noreply.github.com>
2021-07-31 18:26:53 +0300

    Create Task4_6.java

---
+++ src/by/tms/lesson4/Task4_6.java
@@ -0,0 +1,6 @@
+/*
+Дана строка "Versions: java 5, Java 6, Java 7, Java 8, Java 12." Найдите все подстроки "Java X" и распечатать их.
+*/
+
+public class Task4_6 {
+    public static void main(String[] args) {
