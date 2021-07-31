3d3a1d275adebe8e1d57739a27254489a3932467
Aleksandr Kunevich <87695606+AleksandrKunevich@users.noreply.github.com>
2021-07-31 15:03:14 +0300

    Create Task4_3.java

---
+++ src/by/tms/lesson4/Task4_3.java
@@ -0,0 +1,14 @@
+/*
+ * Написать функцию, принимающую в качестве параметров имя, фамилию и отчество, и возвращающую инициалы в 
+ * формате "Ф.И.О." Учесть, что входные параметры могут быть в любом регистре, а результирующая строка должна быть в верхнем.
+ */
+
+public class Task4_3{
+  
+    public static void main(String[] args){
+    
+        System.out.print("Enter your family, name and last name: ");    
+        Scanner scanner = new Scanner(System.in);
+        String enterFIO = scanner.nextLine;
+    }  
+}
