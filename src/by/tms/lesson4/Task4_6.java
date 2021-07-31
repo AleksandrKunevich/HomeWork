1dd6198f025896810c900f144bb46123626604ca
Aleksandr Kunevich <87695606+AleksandrKunevich@users.noreply.github.com>
2021-07-31 19:22:55 +0300

    Update Task4_6.java

--- src/by/tms/lesson4/Task4_6.java
+++ src/by/tms/lesson4/Task4_6.java
@@ -6,32 +6,36 @@ public class Task4_6 {
     
     public static void main(String[] args) {
         
-     String text = "Versions: java 5, JaVa 6, JavA 7, Java 8, Java 12.";
+     String text = "Versions: java 5, JavA X, JaVa 7, Javaa, Java 8, Java 12. JavA,Java 999";
+     
      int count = 0;
      StringBuilder textJava = new StringBuilder();
      
      char[] letters = text.toCharArray();
      for (int i = 0; i < letters.length-1; i++) {
          if (String.valueOf(letters[i]).toUpperCase().equals("J") && (i + 1 < letters.length)) {
-             textJava.append(letters[i]);
              i++;
              if (String.valueOf(letters[i]).toUpperCase().equals("A") && (i + 1 < letters.length)) {
-                 textJava.append(letters[i]);
                  i++;
                  if (String.valueOf(letters[i]).toUpperCase().equals("V") && (i + 1 < letters.length)) {
-                     textJava.append(letters[i]);
                      i++;
                      if (String.valueOf(letters[i]).toUpperCase().equals("A") && (i + 1 < letters.length)) {
-                         textJava.append(letters[i]);
                          i++;
                          if (String.valueOf(letters[i]).toUpperCase().equals(" ") && (i + 1 < letters.length)) {
-                             textJava.append(letters[i]);
                              i++;
+                             int countNumber = 0;
                              for (int j = i; j < letters.length-1; j++, i++) {
                                  if ((letters[i] == '1' || letters[i] == '2' || letters[i] == '3' || letters[i] == '4' || 
                                      letters[i] == '5' || letters[i] == '6' || letters[i] == '7' || letters[i] == '8' || 
                                      letters[i] == '9' || letters[i] == '0') && (i + 1 < letters.length)) {
-                                     textJava.append(letters[i]);
+                                         if (countNumber == 0) {
+                                             textJava.append(letters[i-5]).append(letters[i-4]).append(letters[i-3]).append(letters[i-2]).
+                                             append(letters[i-1]).append(letters[i]);
+                                             countNumber++;
+                                         } else {
+                                             textJava.append(letters[i]);
+                                         }
+                                         
                                      } else {
                                      j = letters.length;
                                      textJava.append(' ');
