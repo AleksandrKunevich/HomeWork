43d707ad1a397fe371165812e8a316ad845ba4e3
Aleksandr Kunevich <87695606+AleksandrKunevich@users.noreply.github.com>
2021-07-31 19:26:39 +0300

    Update Task4_6.java
    Fix bug`s + upgrade check

--- src/by/tms/lesson4/Task4_6.java
+++ src/by/tms/lesson4/Task4_6.java
@@ -12,7 +12,7 @@ public class Task4_6 {
      StringBuilder textJava = new StringBuilder();
      
      char[] letters = text.toCharArray();
-     for (int i = 0; i < letters.length-1; i++) {
+     for (int i = 0; i < letters.length; i++) {
          if (String.valueOf(letters[i]).toUpperCase().equals("J") && (i + 1 < letters.length)) {
              i++;
              if (String.valueOf(letters[i]).toUpperCase().equals("A") && (i + 1 < letters.length)) {
@@ -24,10 +24,10 @@ public class Task4_6 {
                          if (String.valueOf(letters[i]).toUpperCase().equals(" ") && (i + 1 < letters.length)) {
                              i++;
                              int countNumber = 0;
-                             for (int j = i; j < letters.length-1; j++, i++) {
+                             for (int j = i; j < letters.length; j++, i++) {
                                  if ((letters[i] == '1' || letters[i] == '2' || letters[i] == '3' || letters[i] == '4' || 
                                      letters[i] == '5' || letters[i] == '6' || letters[i] == '7' || letters[i] == '8' || 
-                                     letters[i] == '9' || letters[i] == '0') && (i + 1 < letters.length)) {
+                                     letters[i] == '9' || letters[i] == '0')) {
                                          if (countNumber == 0) {
                                              textJava.append(letters[i-5]).append(letters[i-4]).append(letters[i-3]).append(letters[i-2]).
                                              append(letters[i-1]).append(letters[i]);
