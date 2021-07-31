04a18d71a5c7903b9646dd25941f6ed5adb99d2f
Aleksandr Kunevich <87695606+AleksandrKunevich@users.noreply.github.com>
2021-07-31 16:50:59 +0300

    Update Task4_3.java

--- src/by/tms/lesson4/Task4_3.java
+++ src/by/tms/lesson4/Task4_3.java
@@ -7,8 +7,18 @@ public class Task4_3{
   
     public static void main(String[] args){
     
-        System.out.print("Enter your family, name and last name: ");    
+        System.out.print("Enter your family, name and last name (FIO): ");    
         Scanner scanner = new Scanner(System.in);
         String enterFIO = scanner.nextLine;
+        enterFIO = enterFIO.trim();
+        Strind words[] = enterFIO.split(" ");
+        if (words.length < 3) {
+            System.out.println("Incorrect enter FIO!");
+        } else {
+            char famile = words[0].toUpperCase().charAt(0);
+            char name = words[1].toUpperCase().charAt(0);
+            char lastName = words[2].toUpperCase().charAt(0);
+            System.out.println("" + famile + "." + name + "." + lastName + ".");
+        }
     }  
 }
