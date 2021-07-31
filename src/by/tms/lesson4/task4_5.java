8f566cb9ccdad6a12cdd5b410aa067c87e58d571
Aleksandr Kunevich <87695606+AleksandrKunevich@users.noreply.github.com>
2021-07-31 18:16:52 +0300

    Update task4_5.java
    StringBuilder task

--- src/by/tms/lesson4/task4_5.java
+++ src/by/tms/lesson4/task4_5.java
@@ -8,3 +8,58 @@
  * Замените симовл "=" на слово "равно". Используйте методы StringBuilder.replace().
  */
 
+import java.util.Scanner;
+
+public class task4_5{
+  
+   public static void main(String[] args){
+  
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int numberOne = scanner.nextInt();
+        System.out.println("Enter second number: ");
+        int numberTwo = scanner.nextInt();
+             
+        StringBuilder stringPlus = new StringBuilder("" + numberOne + " + " + numberTwo + " = " + (numberOne + numberTwo));
+        StringBuilder stringMinus = new StringBuilder("" + numberOne + " - " + numberTwo + " = " + (numberOne - numberTwo));
+        StringBuilder stringMultiple = new StringBuilder(String.valueOf(numberOne)).append(" - ").
+            append(numberTwo).append(" = ").append((numberOne * numberTwo));
+      
+        System.out.println(stringPlus.toString());
+        System.out.println(stringMinus.toString());
+        System.out.println(stringMultiple.toString() + "\n");
+        
+        int indexPlus = stringPlus.indexOf("=");
+        int indexMinus = stringPlus.indexOf("=");
+        int indexMultiple = stringPlus.indexOf("=");
+        
+        StringBuilder strPlusMod = new StringBuilder(stringPlus);
+        StringBuilder strMinusMod = new StringBuilder(stringMinus);
+        StringBuilder strMultipleMod = new StringBuilder(stringMultiple);
+
+        strPlusMod.deleteCharAt(indexPlus);
+        strMinusMod.deleteCharAt(indexMinus);
+        strMultipleMod.deleteCharAt(indexMultiple);
+        
+        strPlusMod.insert(indexPlus, "равно");
+        strMinusMod.insert(indexMinus, "равно");
+        strMultipleMod.insert(indexMultiple, "равно");
+
+        System.out.println(strPlusMod.toString());
+        System.out.println(strMinusMod.toString());
+        System.out.println(strMultipleMod.toString() + "\n");
+        
+        StringBuilder strPlusModReplace = new StringBuilder(stringPlus);
+        strPlusModReplace.replace(indexPlus, indexPlus + 1, "равно");
+        StringBuilder strMinusModReplace = new StringBuilder(stringMinus);
+        strMinusModReplace.replace(indexMinus, indexMinus + 1, "равно");
+        StringBuilder strMultipleModReplace = new StringBuilder(stringMultiple);
+        strMultipleModReplace.replace(indexMultiple, indexMultiple + 1, "равно");
+        
+        System.out.println(strPlusModReplace.toString());
+        System.out.println(strMinusModReplace.toString());
+        System.out.println(strMultipleModReplace.toString() + "\n");
+    }
+}
+
+  
